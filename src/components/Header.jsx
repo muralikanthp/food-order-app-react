5dd1d9ee1f64c7a9640f093cb5d4182068c54423
Muralikanth Pidikiti <pmkmurali99@gmail.com>
2025-10-17 11:26:13 +0530

    Configurable & Flexible Custom Button Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import logoImg from "../assets/logo.jpg";
 
+import Button from "./UI/Button.jsx";
+
 export default function Header() {
   return (
     <header id="main-header">
@@ -8,7 +10,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
