14efb5be1e8c24b9c05584196f216ab5128c8949
Muralikanth Pidikiti <pmkmurali99@gmail.com>
2025-10-17 12:13:42 +0530

    Cart Context & Reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import logoImg from "../assets/logo.jpg";
+import { useContext } from "react";
 
+import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
+import CartContext from "../store/CartContext.jsx";
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +17,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
