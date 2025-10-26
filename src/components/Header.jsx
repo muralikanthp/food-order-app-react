93f0084558488031dd7b27312619f7b30e030395
Muralikanth Pidikiti <pmkmurali99@gmail.com>
2025-10-26 19:04:56 +0530

    Opening the Cart in the Modal via a New Context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,19 @@ import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
+  const userProgressCtx = useContext(UserProgressContext);
   const cartCtx = useContext(CartContext);
+
   const totalCartItems = cartCtx.items.reduce((total, item) => {
     return total + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -17,7 +24,9 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button onClick={handleShowCart} textOnly>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "", //'cart' | 'checkout'
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export function UserProgressContextProvider({ children }) {
+  const [userProgresss, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+
+  function hideCart() {
+    setUserProgress("");
+  }
+
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgresss,
+    showCart,
+    hideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <UserProgressContext value={userProgressCtx}>
+      {children}
+    </UserProgressContext>
+  );
+}
+
+export default UserProgressContext;
