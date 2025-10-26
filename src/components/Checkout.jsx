2893ef2c30fae3af14f855a6be48429e3c6c9506
Muralikanth Pidikiti <pmkmurali99@gmail.com>
2025-10-26 21:23:51 +0530

    Handling Form Submission & Sending a POST Request with Order Data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -20,13 +20,28 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        order: { items: cartCtx.items, customer: customerData },
+      }),
+    });
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input id="full-name" label="Full Name" type="text" />
+        <Input id="name" label="Name" type="text" />
         <Input id="email" label="E-Mail ID" type="email" />
         <Input id="street" label="Street" type="text" />
         <div className="control-row">
